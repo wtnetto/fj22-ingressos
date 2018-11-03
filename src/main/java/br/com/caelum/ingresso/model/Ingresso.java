3e72601925e4b4cd9c2573300143da6e592b22ff
Netto <neto@gmail.com>
2018-11-03 17:13:26 -0300

    Salvando o  que foi feito ate agora 031118

---
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -0,0 +1,31 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.caelum.ingresso.model.descontos.Desconto;
+
+public class Ingresso {
+	
+	private Sessao sessao;
+	private BigDecimal preco;
+	
+	/**
+	 * @deprecated hibernate only
+	 */
+	
+	public Ingresso(){}
+	
+	public Ingresso (Sessao sessao, Desconto tipoDeDesconto){
+		this.sessao = sessao;
+		this.preco = tipoDeDesconto.aplicarDescontosSobre(sessao.getPreco());
+	}
+	
+	public BigDecimal getPreco(){
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+	
+	public void setPreco(BigDecimal preco){
+		this.preco = preco;
+	}
+}
---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/SemDesconto.java
@@ -0,0 +1,13 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+
+public class SemDesconto implements Desconto{
+
+	@Override
+	public BigDecimal aplicarDescontosSobre(BigDecimal precoOriginal) {
+		// TODO Auto-generated method stub
+		return precoOriginal;
+	}
+
+}
